39fff74a99799936813746fb4c5b5c1eb8788a3f
liangliangyy@gmail.com <liangliangyy@gmail.com>
2015-10-20 23:18:23 +0800

    token

--- wexinrobot.py
+++ wexinrobot.py
@@ -19,8 +19,25 @@ gevent.monkey.patch_all()
 
 robot = werobot.WeRoBot(token='lylinux')
 
+
+from werobot.reply import ArticlesReply, Article
+
+
 @robot.handler
 def echo(message):
-    return 'Hello World!'
+    return [
+        [
+            "title",
+            "description",
+            "img",
+            "url"
+        ],
+        [
+            "逝去日子",
+            "使用django rest framework实现的小工具",
+            "http://www.lylinux.org/%E4%BD%BF%E7%94%A8django-rest-framework%E5%AE%9E%E7%8E%B0%E7%9A%84%E5%B0%8F%E5%B7%A5%E5%85%B7.html",
+            "http://www.lylinux.org/%E4%BD%BF%E7%94%A8django-rest-framework%E5%AE%9E%E7%8E%B0%E7%9A%84%E5%B0%8F%E5%B7%A5%E5%85%B7.html"
+        ]
+    ]
 
-robot.run('gevent',port=8888)
+robot.run('auto',port=8888)
