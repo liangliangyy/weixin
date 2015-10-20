c784ad9bb558e92c0cac4a2ca8224ad69c719488
liangliangyy@gmail.com <liangliangyy@gmail.com>
2015-10-20 22:10:00 +0800

    init

---
+++ wexinrobot.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+
+"""
+@version: ??
+@author: liangliang
+@license: Apache Licence 
+@contact: liangliangyy@gmail.com
+@site: http://www.lylinux.org
+@software: PyCharm
+@file: wexinrobot.py
+@time: 2015/10/20 22:06
+"""
+
+import werobot
+
+robot = werobot.WeRoBot(token='tokenhere')
+
+@robot.handler
+def echo(message):
+    return 'Hello World!'
+
+robot.run()
